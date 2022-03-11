d6d4b194c101f066064e77b870ed5b7cd7f05588
RaphDef <62890716+RaphDef@users.noreply.github.com>
2022-03-11 15:44:52 +0100

    Implemented Calculation (Exercise 3))

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Add EventListener to CalculateButton (Aufgabe3)
+        Button calc = (Button) findViewById(R.id.btnCalc);
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText mat_input = findViewById(R.id.matInput);
+                String mat = mat_input.getText().toString();
+                calculate(mat);
+            }
+        });
+
+
     }
+
+    public void calculate(String mat){
+        int[] matNumbers = new int[mat.length()];
+
+        for(int i = 0; i < matNumbers.length; i++){
+            matNumbers[i] = mat.charAt(i)-48;
+        }
+
+        StringBuilder finalMat = new StringBuilder();
+
+        for(int i = 0; i < matNumbers.length; i++){
+            if(i % 2 == 0){
+                finalMat.append(matNumbers[i]);
+            }else{
+                char temp = (char) (matNumbers[i]+96);
+                finalMat.append(temp);
+            }
+        }
+
+        TextView matOutput = findViewById(R.id.calcText);
+        matOutput.setText(finalMat);
+    }
+
+
 }
